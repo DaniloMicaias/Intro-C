fcbbd052edcf59c4f93602954fb0a19254973ec8
DaniloMicaias <danilo.micaias.sga@gmail.com>
2019-08-12 09:51:47 -0300

    Reworks

--- src/atv9.c
+++ src/atv9.c
@@ -1,8 +1,25 @@
 #include "/home/theking7br/Documentos/itp/inc/console.h"
 
+
+void ordene(int n, int o[]){
+    int aux = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(o[i] > o[j]){
+                aux = o[i];
+                o[i] = o[j];
+                o[j] = aux;
+            }
+        }
+    }
+    print("Lista Ordenada decrescentemente!");
+    print("Qual a posição que você deseja conferir?");
+    int p = readInt();
+    printInt(o[p-1]);
+}
+
 void main(){
     int i, j;
-    int aux = 0;
     int n = readInt();
     int p;
     int a[n];
@@ -12,17 +29,5 @@ void main(){
         a[i] = readInt();
     }
 
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            if(a[i] > a[j]){
-                aux = a[i];
-                a[i] = a[j];
-                a[j] = aux;
-            }
-        }
-    }
-    print("Lista Ordenada decrescentemente!");
-    print("Qual a posição que você deseja conferir?");
-    p = readInt();
-    printInt(a[p-1]);
+    ordene(n, a);
 }
