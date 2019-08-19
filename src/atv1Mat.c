8a91514cc5356972df9b36d63424990a1a0aa356
DaniloMicaias <danilo.micaias.sga@gmail.com>
2019-08-19 10:19:08 -0300

    Reworks

---
+++ src/atv1Mat.c
@@ -0,0 +1,38 @@
+#include "console.h"
+#include <stdio.h>
+
+void main(){
+    int i, j, c, l;
+    l = readInt();
+    c = readInt();
+    int m[l][c];
+    int n[c][l];
+
+    for(i = 0;i < l;i++){
+        for(j = 0;j < c;j++){
+            m[i][j] = readInt();
+        }
+    }
+
+    for(i = 0;i < l;i++){
+        for(j = 0;j < c;j++){
+            n[j][i] = m[i][j];
+        }
+    }
+    print("Matriz Normal:");
+    for(i = 0;i < l;i++){
+        for(j = 0;j < c;j++){
+            printf("%i ", m[i][j]);
+        }
+        print("\n");
+    }
+    print("Matriz Transposta:");
+    for(i = 0;i < l;i++){
+        for(j = 0;j < c;j++){
+            printf("%i ", n[i][j]);
+        }
+        print("\n");
+    }
+
+    
+}
