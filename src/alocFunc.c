af80a04c5baf2784e52a59e42605bb13975f797d
Danilo Micaías <danilo.micaias.sga@gmail.com>
2019-10-29 08:56:41 -0300

    Update alocFunc.c

--- src/alocFunc.c
+++ src/alocFunc.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int matriz(int l, int c){
     int lin, col, **a;
-
+    //alocação
     a = calloc(l, sizeof(int*));
-
     for(lin = 0;lin < l; lin++){
         a[lin] = calloc(c, sizeof(int));
         for(col = 0;col < c; col++){
